9a729a16170725b17a78d9665f31e134801cb109
t113598012 <t113598012@ntut.edu.tw>
2025-12-15 09:56:15 +0800

    feat: 1. compare manual & optimized  2. log route with store

--- src/eval/eval_routes.py
+++ src/eval/eval_routes.py
@@ -8,15 +8,19 @@ class EvalRoutes:
     Notes:
         Evaluate and compare manual and optimized routes.
     """
-    def __init__(self, manual_routes_file, program_routes_file, optimized_routes_file):
+    def __init__(self, manual_routes_file, optimized_routes_file, program_routes_file=None):
         self.manual_routes = self._load_routes(manual_routes_file)
-        self.program_routes = self._load_routes(program_routes_file)
-        self.optimized_routes = self._load_routes(optimized_routes_file)
         self.manual_summary = self._summarize_routes(self.manual_routes)
-        self.program_summary = self._summarize_routes(self.program_routes)
+        self.optimized_routes = self._load_routes(optimized_routes_file)
         self.optimized_summary = self._summarize_routes(self.optimized_routes)
-        self.comparison = self._compare_routes()
-    
+
+        if program_routes_file is not None:
+            self.program_routes = self._load_routes(program_routes_file)
+            self.program_summary = self._summarize_routes(self.program_routes)
+            self.comparison = self._compare_routes()
+        else:
+            self.comparison = self._compare_manual_and_optim()
+
     def _load_routes(self, file_path):
         """
         Notes:
@@ -101,7 +105,7 @@ class EvalRoutes:
     def _compare_routes(self):
         """
         Notes:
-            Compare manual and optimized routes.
+            Compare manual, program and optimized routes.
         
         Args:
             None.
@@ -218,6 +222,109 @@ class EvalRoutes:
         }
 
         df = pd.DataFrame(comparison, index=['manual', 'program', 'optimized']).T
+        return df
+
+
+    def _compare_manual_and_optim(self):
+        """
+        Notes:
+            Compare manual and optimized routes.
+        
+        Args:
+            None.
+        
+        Returns:
+            dict: Comparison results.
+        """
+        comparison = {
+            'vehicle_num' : [
+                self.manual_summary['total_vehicles'], 
+                self.optimized_summary['total_vehicles']
+            ],
+            'vehicle_num(main)' : [
+                self.manual_summary['main_vehicles'], 
+                self.optimized_summary['main_vehicles']
+            ],
+            'vehicle_num(sup)' : [
+                self.manual_summary['support_vehicles'], 
+                self.optimized_summary['support_vehicles']
+            ],
+            'total_store_num' : [
+                self.manual_summary['total_stores'], 
+                self.optimized_summary['total_stores']
+            ],
+            'store_num(main)' : [
+                self.manual_summary['main_stores'], 
+                self.optimized_summary['main_stores']
+            ],
+            'store_num(sup)' : [
+                self.manual_summary['support_stores'], 
+                self.optimized_summary['support_stores']
+            ],
+            'total_dist(km)' : [
+                self.manual_summary['total_distance'], 
+                self.optimized_summary['total_distance']
+            ],
+            'total_time(hr)' : [
+                self.manual_summary['total_duration'], 
+                self.optimized_summary['total_duration']
+            ],
+            'total_main_dist(km)' : [
+                self.manual_summary['total_main_distance'], 
+                self.optimized_summary['total_main_distance']
+            ],
+            'total_main_time(hr)' : [
+                self.manual_summary['total_main_duration'], 
+                self.optimized_summary['total_main_duration']
+            ],
+            'total_sup_dist(km)' : [
+                self.manual_summary['total_support_distance'], 
+                self.optimized_summary['total_support_distance']
+            ],
+            'total_sup_time(hr)' : [
+                self.manual_summary['total_support_duration'], 
+                self.optimized_summary['total_support_duration']
+            ],
+            'avg_dist(km)' : [
+                self.manual_summary['average_distance'],
+                self.optimized_summary['average_distance']
+            ],
+            'avg_time(hr)' : [
+                self.manual_summary['average_duration'], 
+                self.optimized_summary['average_duration']
+            ],
+            'avg_main_dist(km)' : [
+                self.manual_summary['average_main_distance'],
+                self.optimized_summary['average_main_distance']
+            ],
+            'avg_main_time(hr)' : [
+                self.manual_summary['average_main_duration'], 
+                self.optimized_summary['average_main_duration']
+            ],
+            'avg_sup_dist(km)' : [
+                self.manual_summary['average_support_distance'],
+                self.optimized_summary['average_support_distance']
+            ],
+            'avg_sup_time(hr)' : [
+                self.manual_summary['average_support_duration'], 
+                self.optimized_summary['average_support_duration']
+            ],
+            'avg_load_rate' : [
+                self.manual_summary['average_load_rate'], 
+                self.optimized_summary['average_load_rate']
+            ],
+            'avg_main_load_rate' : [
+                self.manual_summary['average_main_load_rate'], 
+                self.optimized_summary['average_main_load_rate']
+            ],
+            'avg_sup_load_rate' : [
+                self.manual_summary['average_support_load_rate'], 
+                self.optimized_summary['average_support_load_rate']
+            ]
+        }
+
+        df = pd.DataFrame(comparison, index=['manual', 'optimized']).T
+
         return df
 
 
--- src/log/log.py
+++ src/log/log.py
@@ -35,6 +35,47 @@ class Log:
             json.dump(record, f, indent=4)
     
 
+    def log_route(self, log_file, routes1, routes2, routes3):
+        """
+        Notes:
+            Log route stores count.
+
+        Args:
+            log_file (str): log file destination.
+            routes1 (dict): routes info.
+            routes2 (dict): routes info.
+            routes3 (dict): routes info.
+        
+        Returns:
+            None.
+        """
+        rows = []
+
+        for route_id in routes1:
+            route1_data = routes1.get(route_id)
+            route2_data = routes2.get(route_id)
+            route3_data = routes3.get(route_id)
+            stores1 = routes1[route_id]['stores'] if route1_data else []
+            stores2 = routes2[route_id]['stores'] if route2_data else []
+            stores3 = routes3[route_id]['stores'] if route3_data else []
+            original_load_rate = stores1[route_id]['dc']['load_rate']
+            maunal_same_code_count = sum(1 for store in stores2 if store['route_code'].startswith(store['route_id']))
+            optimized_same_code_count = sum(1 for store in stores3 if store['route_code'].startswith(store['route_id']))
+
+            rows.append({
+                "route_id": route_id,
+                "original_store_count": f'{len(stores1)}',
+                "original_load_rate": f'{original_load_rate}',
+                "manual_store_count": f'{len(stores2)}({maunal_same_code_count})',
+                "optimized_store_count": f'{len(stores3)}({optimized_same_code_count})',
+            })
+
+        df = pd.DataFrame(rows)
+
+        log_dest = f'{self.log_dir}/{log_file}'
+        df.to_excel(log_dest, index=False)
+
+
     def log_execution(self, log_file, log_data):
         """
         Notes:
