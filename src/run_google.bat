e9c1acfcff65b50b3689b94c8890df7d00f6f513
t113598012 <t113598012@ntut.edu.tw>
2025-12-21 13:52:46 +0800

    feat: 1. run.bat: extend date to 10 days  2. add run_google.bat to adjust time with google maps api  3. add run_test to test the process of system

--- src/run_google.bat
+++ src/run_google.bat
@@ -11,7 +11,7 @@ set DATES=1203 1205 1207 1208 1209 1210 1212 1213 1214 1215
 
 for %%D in (%DATES%) do (
     echo Starting file_date=%%D
-    start "run_%%D" %PYTHON% %SCRIPT% --file_date %%D --seed %SEED%
+    start "run_%%D" %PYTHON% %SCRIPT% --file_date %%D --seed %SEED% --google
 )
 
 echo All jobs started.
