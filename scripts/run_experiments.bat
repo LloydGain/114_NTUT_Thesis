34f6e5b4523126aecb584bf6b9d4cb71b954c4f6
t113598012 <t113598012@ntut.edu.tw>
2026-05-22 21:48:04 +0800

    update: run exp batch

--- scripts/run_experiments.bat
+++ scripts/run_experiments.bat
@@ -4,9 +4,10 @@ setlocal
 
 :: ---[ Settings ]---------------------------------------------
 
-set FILE_DATES=20221203 20221205 20221207
+set FILE_DATES=20260109 20260106 20260102 20230113 20230107 20230102 20221231 20221230 20221227 20221223 20221220 20221213 20221212 20221212 20221210 20221203
 set SEEDS=5
 set GOOGLE_FLAG=--google
+set SKIP_COMPARE_FLAG=--skip_compare
 
 :: ---[ Run ]--------------------------------------------------
 
@@ -17,7 +18,7 @@ for %%D in (%FILE_DATES%) do (
     echo ============================================================
     echo  Running dataset: %%D
     echo ============================================================
-    python "..\tools\run_experiments.py" --file_date %%D --seeds %SEEDS% %GOOGLE_FLAG%
+    python "..\tools\run_experiments.py" --file_date %%D --seeds %SEEDS% %GOOGLE_FLAG% %SKIP_COMPARE_FLAG%
     if errorlevel 1 (
         echo [ERROR] Dataset %%D failed. Continuing to next...
     )
