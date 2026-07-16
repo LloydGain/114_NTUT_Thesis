6e818637aa8c76a5aa69ec3c375207395d7f8dc5
qwe8496516 <jeff35581601@gmail.com>
2026-07-16 11:48:04 +0800

    update: osrm setup

--- scripts/setup/setup_osrm.bat
+++ scripts/setup/setup_osrm.bat
@@ -12,10 +12,14 @@ if not exist "%DATA_DIR%\%MAP%.osm.pbf" (
     powershell Invoke-WebRequest -Uri http://download.geofabrik.de/asia/%MAP%.osm.pbf -OutFile "%DATA_DIR%\%MAP%.osm.pbf"
 )
 
-docker run --rm -v "%DATA_DIR%:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/%MAP%.osm.pbf
+if not exist "%DATA_DIR%\%MAP%.osrm" (
+    echo Building OSRM graph...
+    docker run --rm -v "%DATA_DIR%:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/%MAP%.osm.pbf
+    docker run --rm -v "%DATA_DIR%:/data" osrm/osrm-backend osrm-partition /data/%MAP%.osrm
+    docker run --rm -v "%DATA_DIR%:/data" osrm/osrm-backend osrm-customize /data/%MAP%.osrm
+) else (
+    echo OSRM graph already exists. Skipping build.
+)
 
-docker run --rm -v "%DATA_DIR%:/data" osrm/osrm-backend osrm-partition /data/%MAP%.osrm
-
-docker run --rm -v "%DATA_DIR%:/data" osrm/osrm-backend osrm-customize /data/%MAP%.osrm
-
+echo Starting OSRM server...
 docker run --rm -p 5000:5000 -v "%DATA_DIR%:/data" osrm/osrm-backend osrm-routed --algorithm mld /data/%MAP%.osrm
